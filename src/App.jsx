d503feec1e931e6b9d60d5f415269a58e4b15617
Mohammed Ghunaim <mohammed.w.ghunaim@gmail.com>
2025-02-11 00:05:35 +0300

    added icons and pngs and rendered search bar header and image with background and search bar takes input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import Search from './components/Search.jsx'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   return (
     <main>
       <div className="pattern" />
 
       <div className="wrapper">
         <header>
-          <h1>Find Movies You'll Like Without Hassle</h1>
+          <img src="/hero.png" alt="Hero Banner"/>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Like Without Much Work</h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
       </div>
     </main>
   )
